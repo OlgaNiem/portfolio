449c4d9bc6eefb92488c40c04fb2e811f13d5cd6
OlgaNiem <olhaniemiets@gmail.com>
2024-11-01 10:58:13 +0200

    update tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import typography from "@tailwindcss/typography"; 
 
 const config: Config = {
   content: [
@@ -9,7 +10,8 @@ const config: Config = {
   ],
   theme: {},
   plugins: [
-    require("@tailwindcss/typography")
+    typography, 
   ],
 };
+
 export default config;
